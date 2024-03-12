e4866c8b9cb3a2114d420713c32dc72ddedd27c2
YS Liu <seanly@opsbox.dev>
2024-03-12 01:43:58 +0000

    rke2up support arm64

--- cluster/rke2up/lib/download.sh
+++ cluster/rke2up/lib/download.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
+
 _rke2_version=${1:-v1.26.10+rke2r1}
 shift
 
@@ -15,7 +18,7 @@ shift
 ## - rke2-images-calico.linux-amd64.tar.gz
 
 _release_baseurl=https://github.com/rancher/rke2/releases/download
-_images_list_file=rke2-images-all.linux-amd64.txt
+_images_list_file=rke2-images-all.linux-${ARCH}.txt
 _url_encode_release=$(echo "$_rke2_version"|sed 's/+/%2B/g')
 
  _release_dir=$(pwd)/$_rke2_version
@@ -23,35 +26,35 @@ _url_encode_release=$(echo "$_rke2_version"|sed 's/+/%2B/g')
  mkdir -p ${_release_dir}/artifacts
 
 do_download_verify_file() {
-    _sha256sum_file=sha256sum-amd64.txt
+    _sha256sum_file=sha256sum-${ARCH}.txt
 
     _target_url=${_release_baseurl}/${_url_encode_release}/${_sha256sum_file}
     curl -sfL $_target_url > ${_release_dir}/artifacts/${_sha256sum_file}
 }
 
 do_download_rke2() {
-    _rke2_package_file=rke2.linux-amd64.tar.gz
+    _rke2_package_file=rke2.linux-${ARCH}.tar.gz
 
     _target_url=${_release_baseurl}/${_url_encode_release}/${_rke2_package_file}
     curl -sfL $_target_url > ${_release_dir}/artifacts/${_rke2_package_file}
 }
 
 do_download_rke2_core_images() {
-    _rke2_core_images=rke2-images-core.linux-amd64.tar.gz
+    _rke2_core_images=rke2-images-core.linux-${ARCH}.tar.gz
 
     _target_url=${_release_baseurl}/${_url_encode_release}/${_rke2_core_images}
     curl -sfL $_target_url > ${_release_dir}/images/${_rke2_core_images}
 }
 
 do_download_rke2_calico_images() {
-    _rke2_calico_images=rke2-images-calico.linux-amd64.tar.gz
+    _rke2_calico_images=rke2-images-calico.linux-${ARCH}.tar.gz
 
     _target_url=${_release_baseurl}/${_url_encode_release}/${_rke2_calico_images}
     curl -sfL $_target_url > ${_release_dir}/images/${_rke2_calico_images}
 }
 
 do_download_rke2_cilium_images() {
-    _rke2_cilium_images=rke2-images-cilium.linux-amd64.tar.gz
+    _rke2_cilium_images=rke2-images-cilium.linux-${ARCH}.tar.gz
 
     _target_url=${_release_baseurl}/${_url_encode_release}/${_rke2_cilium_images}
     curl -sfL $_target_url > ${_release_dir}/images/${_rke2_cilium_images}
